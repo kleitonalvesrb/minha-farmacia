fe56495d592a61d98bf6520455a0f088317faec7
Kleiton Batista <kleiton.market@gmail.com>
2016-07-28 20:28:16 -0300

    - Foram adicionados as frameworks do parse e do facebook - Foram feito todas as devidas configurações do para a utilização do parse como o back-end - Foram feito todas as configurações necessárias para a conexão com o facebook - Rotina de teste de autenticação no facebook

--- Minha Farmacia/ViewController.swift
+++ Minha Farmacia/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
-
+import Parse
+import ParseFacebookUtilsV4
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Iniciando o app")
+        let permissions = ["public_profile"]
+        PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions) { (user: PFUser?, error: NSError?) in
+            if let erro = error{
+                print(erro)
+            }else{
+                if let usuario = user{
+                    print(usuario)
+                }
+            }
+        }
+        print(PFUser.currentUser()?.username)
+
     }
 
     override func didReceiveMemoryWarning() {

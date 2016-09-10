80cbad2cfc3d4de25c453d52fd3393bfb2bdc9cd
kleitonbatista <kleiton.a.batista@gmail.com>
2016-09-10 00:49:56 -0300

    - Criado a classe dosagem Medicamento

---
+++ Minha Farmacia/DosagemMedicamento.swift
@@ -0,0 +1,17 @@
+//
+//  DosagemMedicamento.swift
+//  Farmácia
+//
+//  Created by Kleiton Batista on 10/09/16.
+//  Copyright © 2016 Kleiton Batista. All rights reserved.
+//
+
+import UIKit
+
+class DosagemMedicamento: NSObject {
+    var tipoMedicamento: String!
+    var dosagem: String!
+    var intervaloDose: Int!
+    var periodoTratamento: Int!
+
+}
